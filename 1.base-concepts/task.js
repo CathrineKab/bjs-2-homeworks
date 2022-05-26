73244556669dc9d469cbbdd68d9ca596029cd211
CathrineKab <99976784+CathrineKab@users.noreply.github.com>
2022-05-26 14:37:47 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,7 +5,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b**2 - 4*a*c;
-
+  
   if (d > 0) {
     arr.push((-b + Math.sqrt(d))/(2*a));
     arr.push((-b - Math.sqrt(d))/(2*a));
@@ -21,8 +21,7 @@ function solveEquation(a, b, c) {
 //задача 2
 
 function calculateTotalMortgage (percent, contribution, amount, date) {
-  let totalAmount;
-
+ let totalAmount;
  let d = new Date();
   percent = percent / 100;
   let P = percent / 12;
@@ -39,7 +38,6 @@ function calculateTotalMortgage (percent, contribution, amount, date) {
   if ((amount <= 0) || (isNaN(amount))) {
     return 'Параметр "Общая стоимость" содержит неправильное значение "test"';
     } 
-
   if (date <= d) {
     return 'Срок ипотеки не может быть меньше или равен текущей даты!';
   } else if (isNaN(date)) {
@@ -48,8 +46,6 @@ function calculateTotalMortgage (percent, contribution, amount, date) {
 
   monthlyFee = creditPrincipal*(P + (P/(((1 + P)**months) - 1)));
   totalAmount = monthlyFee * months;
-
-
   return Number (totalAmount.toFixed(2));
   console.log(`Общая сумма по ипотеке составляет ${totalAmount}`);
 }
