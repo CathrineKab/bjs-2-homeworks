60b2b5bfb6299dcb9ce557cda6d6added0f29289
CathrineKab <99976784+CathrineKab@users.noreply.github.com>
2022-06-02 16:10:32 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
+//Задача 1
+
 function compareArrays(arr1, arr2) {
-  let result;
+  let result =  arr1.length === arr2.length && arr1.every((number, index) => number === arr2[index]);
 
-  // Ваш код
-
-  return result; // boolean
+  return result;
 }
 
+
+//Задача 2
+
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((number) => number >= 0).filter((number) => number % 3 == 0).map((number) => number * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr; 
 }
