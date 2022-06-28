4ecc302f02d6e7a68478085ee56679d20f0830f8
CathrineKab <99976784+CathrineKab@users.noreply.github.com>
2022-06-28 15:00:38 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,69 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    let result;
+    let key = args.join(',');
+    const hash = {key, result}; 
+    let idx = cache.findIndex((itm) => key === itm.key);
+    let newCache = []; 
+  //если элемент наден
+  if (idx !== -1){
+    console.log("Из кэша: " + cache[idx].result);
+    return "Из кэша: " + cache[idx].result;
+  } else {
+  //вычисляем значение
+  result = func(...args);
+  hash.result = result;
+  cache.push(hash);
+  if (cache.length > 5){ 
+   newCache = cache.filter((itm) => itm != cache[0])
+   cache = newCache;
+ }
+ console.log("Вычисляем: " + result);
+ return "Вычисляем: " + result;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+}
+return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let flag = false;
+
+  function wrapper(...args) {
+    if(flag === false){
+      func(...args);
+      flag = true;
+      timeout = setTimeout(() => {
+        flag = false;
+      }, ms)
+    } else {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {flag = false}, ms);
+    }
+  }
+  return wrapper;
+}
+
+function debounceDecorator2(func, ms) {
+  let timeout;
+  let flag = false;
+  wrapper.count = 0;
+  function wrapper(...args) {
+    wrapper.count = wrapper.count + 1;
+    if(flag === false){
+      func(...args);
+      flag = true;
+      timeout = setTimeout(() => {
+        flag = false;
+      }, ms)
+    } else {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {flag = false}, ms);
+    }
+  }
+  wrapper.count;
+  return wrapper;
 }
